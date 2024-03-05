6f1860079efc3b9e135edd88608dc3c4faa621dd
purekite <soocy0718@gmail.com>
2024-03-05 19:22:15 +0900

    build 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Main {
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.info("hello world!");
-    }
+	public static void main(String[] args) {
+	}
 }
--- src/test/java/GithubActionSuccessTest.java
+++ src/test/java/GithubActionSuccessTest.java
@@ -1,10 +1,10 @@
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class GithubActionSuccessTest {
-    @Test
-    @Tag("IntegrationTest")
-    void testSuccess(){
-        assert(true);
-    }
+class GithubActionSuccessTest {
+	@Test
+	@Tag("IntegrationTest")
+	void testSuccess() {
+		assert (true);
+	}
 }
