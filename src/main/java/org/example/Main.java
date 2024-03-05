94d09d427336e65aa898e39d36b202b796890b83
purekite <soocy0718@gmail.com>
2024-03-05 18:16:49 +0900

    ci checkstyle test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Main.class);
         logger.info("hello world!");
+        logger.info("hello world!");
     }
 }
